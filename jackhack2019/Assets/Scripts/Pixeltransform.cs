76c00b1d97fedcb732b1e1117c02f7a34de37618
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-05-04 11:43:12 +0900

    Texture2Dの座標をワールド座標に変換するスクリプトを追加

---
+++ jackhack2019/Assets/Scripts/Pixeltransform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pixeltransform : MonoBehaviour
+{
+    //canvasのカメラの座標
+    static Camera CanvasCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanvasCamera = GetComponent<Canvas>().worldCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //pixel座標をワールド座標に変換します
+    public static Vector3 Getworldpositionfromrectposition(RectTransform rect)
+    {
+        //UI座標からスクリーン座標に変換
+        Vector2 screenpos = RectTransformUtility.WorldToScreenPoint(CanvasCamera,rect.position);
+
+        //代入する用のワールド座標
+        Vector3 result = Vector3.zero;
+
+        //スクリーン座標からワールド座標に変換
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rect,screenpos,CanvasCamera,out result);
+
+        return result;
+    }
+
+}
