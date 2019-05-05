f3793399d5e978b9ba5645b53317464ef4f0d810
DESKTOP-APK21IT\itoka229 <itoka229@yahoo.co.jp>
2019-05-05 12:06:00 +0900

    Textの生成、移動スクリプトの追加

---
+++ jackhack2019/Assets/Scripts/InstantiateText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OpenCvSharp;
+using UnityEngine;
+
+public class InstantiateText : MonoBehaviour
+{
+    public static GameObject Text;
+    public static GameObject Canvas;
+    public GameObject Text2;//ここにinstantiateするテキストを入れる
+    public GameObject Canvas2;//ここに親となるCanvasを入れる
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = Text2;
+        Canvas = Canvas2;
+    }
+
+    public static GameObject Instxt(RectTransform rt,OpenCvSharp.Rect r)
+    {
+        var newVec = new Vector2((float)r.X, -(float)r.Y) - new Vector2(rt.sizeDelta.x / 2.0f, -rt.sizeDelta.y / 2.0f);
+        GameObject a = GameObject.Instantiate(Text, Canvas.transform.parent);
+        a.transform.localPosition = (newVec + new Vector2((float)r.Width / 2.0f, 0)) * Canvas.transform.localScale.x;
+        return Text;
+    }
+}
